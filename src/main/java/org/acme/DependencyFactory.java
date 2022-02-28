521bdce934fdfc0a1cf5cfd50742b389bba9086c
Vaibhav Ghadage <vaibhav.ghadage@ibm.com>
2022-02-28 12:54:18 +0530

    code from test addd

---
+++ src/main/java/org/acme/DependencyFactory.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.acme;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+// import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.metrics.LoggingMetricPublisher;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+/**
+ * The module containing all dependencies required by the {@link App}.
+ */
+public class DependencyFactory {
+
+    /** Default Properties Credentials file path. */
+    private static final String TEST_CREDENTIALS_PROFILE_NAME = "aws-test-account";
+
+    public static final AwsCredentialsProviderChain CREDENTIALS_PROVIDER_CHAIN = AwsCredentialsProviderChain.of(
+            ProfileCredentialsProvider.builder()
+                    .profileName(TEST_CREDENTIALS_PROFILE_NAME)
+                    .build(),
+            DefaultCredentialsProvider.create());
+
+    private DependencyFactory() {
+    }
+
+    // public static S3Client s3UrlConnectionHttpClient() {
+    // return S3Client.builder()
+    // .credentialsProvider(CREDENTIALS_PROVIDER_CHAIN)
+    // .httpClientBuilder(UrlConnectionHttpClient.builder())
+    // .overrideConfiguration(o ->
+    // o.addMetricPublisher(LoggingMetricPublisher.create()))
+    // .region(Region.US_WEST_2)
+    // .build();
+    // }
+
+    // public static S3Client s3ApacheHttpClient() {
+    // return S3Client.builder()
+    // .credentialsProvider(CREDENTIALS_PROVIDER_CHAIN)
+    // .httpClientBuilder(ApacheHttpClient.builder())
+    // .overrideConfiguration(o ->
+    // o.addMetricPublisher(LoggingMetricPublisher.create()))
+    // .region(Region.US_WEST_2)
+    // .build();
+    // }
+
+    // public static S3AsyncClient s3NettyClient() {
+    // return S3AsyncClient.builder()
+    // .credentialsProvider(CREDENTIALS_PROVIDER_CHAIN)
+    // .overrideConfiguration(o ->
+    // o.addMetricPublisher(LoggingMetricPublisher.create()))
+    // .region(Region.US_WEST_2)
+    // .build();
+    // }
+
+    public static DynamoDbClient ddbClient() {
+        return DynamoDbClient.builder()
+                .credentialsProvider(CREDENTIALS_PROVIDER_CHAIN)
+                .httpClientBuilder(ApacheHttpClient.builder())
+                .overrideConfiguration(o -> o.addMetricPublisher(LoggingMetricPublisher.create()))
+                .region(Region.US_WEST_2)
+                .build();
+    }
+
+    public static DynamoDbEnhancedClient enhancedClient(DynamoDbClient ddbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(ddbClient)
+                .build();
+
+    }
+}
--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,80 +5,142 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
-import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
-
 import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.primaryPartitionKey;
 import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.primarySortKey;
+import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.secondaryPartitionKey;
+import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.secondarySortKey;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 
 @Path("/hello")
 public class GreetingResource {
+        private static final String TABLE_NAME = "native-image" + System.currentTimeMillis();
+        private static final String ATTRIBUTE_NAME_WITH_SPECIAL_CHARACTERS = "a*t:t.r-i#bute3";
+        // private static final Logger logger =
+        // LoggerFactory.getLogger(DynamoDbEnhancedClientTestRunner.class);
+
+        private static final TableSchema<Record> TABLE_SCHEMA = StaticTableSchema.builder(Record.class)
+                        .newItemSupplier(Record::new)
+                        .addAttribute(String.class, a -> a.name("id")
+                                        .getter(Record::getId)
+                                        .setter(Record::setId)
+                                        .tags(primaryPartitionKey()))
+                        .addAttribute(String.class, a -> a.name("sort")
+                                        .getter(Record::getSort)
+                                        .setter(Record::setSort)
+                                        .tags(primarySortKey()))
+                        .addAttribute(String.class, a -> a.name("attribute")
+                                        .getter(Record::getAttribute)
+                                        .setter(Record::setAttribute))
+                        .addAttribute(String.class, a -> a.name("attribute2*")
+                                        .getter(Record::getAttribute2)
+                                        .setter(Record::setAttribute2)
+                                        .tags(secondaryPartitionKey("gsi_1")))
+                        .addAttribute(String.class, a -> a.name(ATTRIBUTE_NAME_WITH_SPECIAL_CHARACTERS)
+                                        .getter(Record::getAttribute3)
+                                        .setter(Record::setAttribute3)
+                                        .tags(secondarySortKey("gsi_1")))
+                        .build();
+
+        // private static final ProvisionedThroughput DEFAULT_PROVISIONED_THROUGHPUT =
+        // ProvisionedThroughput.builder()
+        // .readCapacityUnits(50L)
+        // .writeCapacityUnits(50L)
+        // .build();
+
+        private final DynamoDbClient dynamoDbClient;
+        private final DynamoDbEnhancedClient enhancedClient;
+
+        public GreetingResource() {
+                dynamoDbClient = DependencyFactory.ddbClient();
+                enhancedClient = DependencyFactory.enhancedClient(dynamoDbClient);
+        }
+
+        private static final class Record {
+                private String id;
+                private String sort;
+                private String attribute;
+                private String attribute2;
+                private String attribute3;
+
+                private String getId() {
+                        return id;
+                }
+
+                private Record setId(String id) {
+                        this.id = id;
+                        return this;
+                }
+
+                private String getSort() {
+                        return sort;
+                }
+
+                private Record setSort(String sort) {
+                        this.sort = sort;
+                        return this;
+                }
+
+                private String getAttribute() {
+                        return attribute;
+                }
+
+                private Record setAttribute(String attribute) {
+                        this.attribute = attribute;
+                        return this;
+                }
+
+                private String getAttribute2() {
+                        return attribute2;
+                }
+
+                private Record setAttribute2(String attribute2) {
+                        this.attribute2 = attribute2;
+                        return this;
+                }
+
+                private String getAttribute3() {
+                        return attribute3;
+                }
+
+                private Record setAttribute3(String attribute3) {
+                        this.attribute3 = attribute3;
+                        return this;
+                }
+        }
 
         @GET
         @Produces(MediaType.TEXT_PLAIN)
         public String hello() {
-                Region region = Region.AP_SOUTH_1;
-                DynamoDbClient ddb = DynamoDbClient.builder()
-                                .credentialsProvider(DefaultCredentialsProvider.create())
-                                .region(region)
-                                .build();
+                // Region region = Region.AP_SOUTH_1;
+                // DynamoDbClient ddb = DynamoDbClient.builder()
+                // .credentialsProvider(DefaultCredentialsProvider.create())
+                // .region(region)
+                // .build();
 
-                DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
-                                .dynamoDbClient(ddb)
-                                .build();
+                // DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                // .dynamoDbClient(ddb)
+                // .build();
                 try {
                         // Create a DynamoDbTable object
-                        DynamoDbTable<Employee> mappedTable = enhancedClient.table("Employee",
-                                        StaticTableSchema.builder(Employee.class)
-                                                        .newItemSupplier(Employee::new)
-                                                        .addAttribute(String.class, a -> a.name("name")
-                                                                        .getter(Employee::getName)
-                                                                        .setter(Employee::setName)
-                                                                        .tags(primaryPartitionKey()))
-                                                        .addAttribute(Integer.class, a -> a.name("age")
-                                                                        .getter(Employee::getAge)
-                                                                        .setter(Employee::setAge)
-                                                                        .tags(primarySortKey()))
-                                                        // .addAttribute(String.class, a -> a.name("name")
-                                                        // .getter(Customer::getName)
-                                                        // .setter(Customer::setName)
-                                                        // .tags(secondaryPartitionKey("customers_by_name")))
-                                                        // .addAttribute(Instant.class, a -> a.name("created_date")
-                                                        // .getter(Customer::getCreatedDate)
-                                                        // .setter(Customer::setCreatedDate)
-                                                        // .tags(secondarySortKey("customers_by_date"),
-                                                        // secondarySortKey("customers_by_name")))
-                                                        .build());
+                        DynamoDbTable<Record> mappedTable = enhancedClient.table(TABLE_NAME, TABLE_SCHEMA);
 
-                        // Create a KEY object
-                        // System.out.println(mappedTable.toString());
-                        Key key = Key.builder()
-                                        .partitionValue("vaibhav")
-                                        .sortValue(22)
-                                        .build();
+                        Record record = new Record()
+                                        .setId("id-value")
+                                        .setSort("sort-value")
+                                        .setAttribute("one")
+                                        .setAttribute2("two")
+                                        .setAttribute3("three");
 
-                        // System.out.println(key.toString());
-                        // Map<String, AttributeValue> key1 = new HashMap<>();
-                        // key1.put("name", AttributeValue.builder().s("vaibhav").build());
+                        mappedTable.putItem(r -> r.item(record));
 
-                        // key1.put("age", AttributeValue.builder().n("22").build());
-
-                        // return GetItemRequest.builder()
-                        // .tableName("Employee")
-                        // .key(key1)
-                        // // .attributesToGet(FRUIT_NAME_COL, FRUIT_DESC_COL)
-                        // .build().toString();
-                        // Get the item by using the key
-                        Employee result = mappedTable.getItem(key);
+                        Record result = mappedTable
+                                        .getItem(r -> r.key(k -> k.partitionValue("id-value").sortValue("sort-value")));
                         return "The email value is " + result.toString();
 
                 } catch (DynamoDbException e) {
